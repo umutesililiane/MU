3a5c293fd9c70b4b1745a1d835004a948b44c88b
umutesililiane <91309873+umutesililiane@users.noreply.github.com>
2023-02-26 01:24:41 +0530

    adding a typography component and making cards look prettier

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 
 import React from "react";
-import "./App.css"; 
+import "./App.css";
 import TourCard from "./components/TourCard";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 
 function App() {
-    return(
+    return (
         <div className="App">
-           <Container>
-              <Grid container  spacing={5} > 
-                <TourCard/>
-                <TourCard/>
-                <TourCard/>
-                <TourCard/>
-               </Grid>
-           </Container>  
-            
+            <Container>
+                <Grid container spacing={5} >
+                    <TourCard />
+                    <TourCard />
+                    <TourCard />
+                    <TourCard />
+                </Grid>
+            </Container>
+
         </div>
     )
 }
