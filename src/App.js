3ccf85cac0b3196929c3754444fca4284875d516
umutesililiane <91309873+umutesililiane@users.noreply.github.com>
2023-02-25 14:52:17 +0530

    adding first commponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import "./App.css"; 
+import TourCard from "./components/TourCard";
 
 function App() {
     return(
         <div className="App">
-            
+            <TourCard/>
         </div>
     )
 }
---
+++ src/components/TourCard.js
@@ -0,0 +1,11 @@
+
+import Paper from '@mui/material/Paper';
+
+
+const TourCard = () => {
+    return (
+        <Paper elevation = {10} >hello , Lily</Paper>
+    )
+}
+
+export default TourCard;
